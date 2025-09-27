ae3b7043dd1de0246fc3e88cdba1a4baf1d114e2
Aroh oluchi <perpysylvie@gmail.com>
2025-09-27 23:26:15 +0100

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,7 +11,8 @@ const about = {
   description:`I’m a frontend engineer who enjoys building clean, responsive, and user-friendly websites using HTML5, CSS, JavaScript, and React. I focus on turning designs into smooth, functional interfaces that look great on any device. I care about writing code that’s organized, easy to maintain, and works well across different browsers. I enjoy working with others to bring ideas to life and create digital experiences that are both useful and enjoyable for users.
 `,
   
-  resume: '/oluchitechcv2.pdf',
+ 
+//  resume:  '/taste tracker.png',
   social: {
     linkedin: 'https://www.linkedin.com/in/oluchukwuaroh?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
     github: 'https://github.com/Peps2/EHYA',
@@ -22,16 +23,19 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'TasteTracker–Smart Food Discovery Platform(Coming Soon)',
+    name: 'TasteTracker–Smart Food Discovery Platform(Ongoing)',
     description:
       `Currently building a React-based web app that helps users discover and track their favorite dishes.
        Features include a clean, component-driven architecture, real-time recommendations via WebSockets, live search, AI-driven suggestions, infinite scrolling, and list virtualization for smooth performance.`,
     stack: ['Javascript', 'CSS', 'HTML', 'React.js','Tailwind CSS'],
-    sourceCode: 'https://github.com',
-    // livePreview: 'https://github.com',
+    // sourceCode: '/taste tracker.png',
+      livePreview: '/taste tracker.png',
+    
+   
   },
   
 ]
+
 
 const skills = [
   // skills can be added or removed
